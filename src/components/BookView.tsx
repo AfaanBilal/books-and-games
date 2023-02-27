5ec3a5e7e2babe1196b5a56d850bdf1fcb23e18a
Afaan Bilal <afaanbilal@gmail.com>
2023-02-27 08:42:30 +0530

    ✨ Accessibility: ALT

--- src/components/BookView.tsx
+++ src/components/BookView.tsx
@@ -13,7 +13,7 @@ import { Book } from "../books";
 const BookView: Component<{ book: Book }> = (props) => {
     return (
         <div class="min-w-[250px] max-w-[250px] py-6 flex flex-col items-center justify-between gap-4 border rounded-xl border-slate-700 bg-slate-900">
-            <div><img class="h-[250px]" src={props.book.cover} /></div>
+            <div><img class="h-[250px]" src={props.book.cover} alt={props.book.title} /></div>
             <div class="text-lg text-slate-300 text-center font-bold px-4">{props.book.title}</div>
         </div>
     );
